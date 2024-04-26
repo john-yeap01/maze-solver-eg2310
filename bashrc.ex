506b090ecb71975c0296fd84c26a45f64e5111c5
john-yeap01 <johnyeap01@gmail.com>
2024-04-26 02:41:16 -0400

    added and deleted in the bashrc files -- for computer and rpi

--- bashrc.ex
+++ bashrc.ex
@@ -1,18 +1,13 @@
-alias cw='cd ~/colcon_ws'
-alias cs='cd ~/colcon_ws/src'
-alias ca='cd ~/colcon_ws/src/auto_nav/auto_nav'
-
 alias cban='cd ~/colcon_ws && colcon build --packages-select auto_nav'
 alias si='source install/setup.bash'
 alias cb='cd ~/colcon_ws && colcon build --symlink-install && source ~/.bashrc'
-source /opt/ros/foxy/setup.bash
-source ~/colcon_ws/install/local_setup.bash
-
 alias cw='cd ~/colcon_ws'
 alias cs='cd ~/colcon_ws/src'
 alias cb='cd ~/colcon_ws && colcon build --symlink-install && source ~/.bashrc'
+
 source /opt/ros/foxy/setup.bash
 source ~/colcon_ws/install/local_setup.bash
+
 export ROS_DOMAIN_ID=30 #TURTLEBOT3
 export ROS_DOMAIN_ID=38 #TURTLEBOT3
 
@@ -23,3 +18,6 @@ alias rteleop='ros2 run turtlebot3_teleop teleop_keyboard'
 alias rslam='ros2 launch turtlebot3_cartographer cartographer.launch.py'
 
 export GAZEBO_MODEL_PATH=~/colcon_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models
+
+
+
